4aa43f24d8bd577445afee774b85f59d4d827c66
rin <watanabesmall@gmail.com>
2023-01-10 19:53:11 +0800

    support date range

--- func/between.py
+++ func/between.py
@@ -1,6 +1,15 @@
 import random
+from datetime import datetime
 
-
+def trans_to_date(date) -> datetime:
+        try:
+            if ':' in date:
+                t = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+            else:
+                t = datetime.strptime(date, '%Y-%m-%d')
+            return t
+        except:
+            return None 
 class Between():
     def __init__(self, candidates) -> None:
         self.candidates = candidates
@@ -10,5 +19,14 @@ class Between():
             return
         fisrt = self.candidates[0]
         second = self.candidates[1]
-        idx = random.randrange(fisrt, second)
-        return idx
+        if isinstance(fisrt, int) and isinstance(second, int):
+            return random.randrange(fisrt, second)
+        if isinstance(fisrt, str) and isinstance(second, str):
+                first_time = trans_to_date(fisrt)
+                second_time = trans_to_date(second)
+                if first_time and second_time:
+                    f = first_time.timestamp()
+                    s = second_time.timestamp()
+                    ts = random.randrange(f,s)
+                    datetime.fromtimestamp(ts)
+                    return datetime.fromtimestamp(ts)
